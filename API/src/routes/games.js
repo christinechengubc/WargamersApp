0a083e7b242c20060dfde1d8f8911dd8d11351fd
crystal-ck7 <crystal.ck7@gmail.com>
2018-03-20 20:06:26 -0700

    added search game queries

--- API/src/routes/games.js
+++ API/src/routes/games.js
@@ -17,4 +17,45 @@ games.get('/', (req, res) => {
 		});
 });
 
+games.get('/has-genre/:genre', (req, res) => {
+  var genre = req.params.genre;
+  var sql = 'SELECT gametitle FROM game, hasgenre WHERE gametitle = title AND genrename =' + genre;
+db.any(sql)
+  .then(function (data) {
+
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved games with genre ' + genre
+      });
+  })
+  .catch(function (err) {
+    console.error("Error when retrieving games " + err);
+  });
+});
+
+games.get('/title/:partTitle', (req, res) => {
+  var partTitle = req.params.partTitle;
+  partTitle = partTitle.replace(/\'/g, "");
+
+var sql = 'SELECT title FROM game WHERE title LIKE \'%' + partTitle + '%\'';
+db.any(sql)
+  .then(function (data) {
+
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved games with partial title ' + partTitle
+      });
+  })
+  .catch(function (err) {
+    console.log(sql);
+    console.error("Error when retrieving games " + err);
+  });
+});
+
+
+
 module.exports = games;
