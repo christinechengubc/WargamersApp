7a6157d609e8b5c6492490951d84018c76cddf6c
christinechengubc <christine.lyn.cheng@gmail.com>
2018-03-24 20:23:00 -0700

    added events info and events create page

--- client/src/pages/events/events.ts
+++ client/src/pages/events/events.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { TranslateService } from '@ngx-translate/core';
 
 /**
  * Generated class for the EventsPage page.
@@ -15,7 +17,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  events: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public translateService: TranslateService) {
+    translateService.get('DATABASE_URL').subscribe(value => {
+      this.http.get(value + '/events').map(res => res.json()).subscribe(
+        data => {
+          this.events = data.data;
+          console.log("now logging");
+          console.log(data.data);
+        },
+        err => {
+          console.log("Oops!");
+          console.log(err);
+        }
+      );
+    });
+  }
+
+  eventInfo(name, date) {
+    this.navCtrl.push('EventInfoPage', {
+      eventName: name,
+      eventDate: date
+    });
+  }
+
+  addEvent() {
+    this.navCtrl.push('EventCreatePage');
   }
 
   ionViewDidLoad() {
