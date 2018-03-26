0bdba0814ebefe50fb29f40c0237b595c15cbda0
christinechengubc <christine.lyn.cheng@gmail.com>
2018-03-25 19:18:29 -0700

    refactor code to use url.ts instead of translate service

--- client/src/pages/games/games.ts
+++ client/src/pages/games/games.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
-import { TranslateService } from '@ngx-translate/core';
+import { API_URL } from '../url';
 import { PopoverPage } from '../popover/popover';
 import { Http } from '@angular/http';
 
@@ -20,21 +20,18 @@ export class GamesPage {
 
   games: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtr: PopoverController, public http: Http, public translateService: TranslateService) {
-    translateService.get('DATABASE_URL').subscribe(value => {
-      this.http.get(value + '/games').map(res => res.json()).subscribe(
-        data => {
-          this.games = data.data;
-          console.log("now logging");
-          console.log(data.data);
-        },
-        err => {
-          console.log("Oops!");
-          console.log(err);
-        }
-      );
-    });
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtr: PopoverController, public http: Http) {
+    this.http.get(API_URL + '/games').map(res => res.json()).subscribe(
+      data => {
+        this.games = data.data;
+        console.log("now logging");
+        console.log(data.data);
+      },
+      err => {
+        console.log("Oops!");
+        console.log(err);
+      }
+    );
   }
 
   presentPopover(event) {
