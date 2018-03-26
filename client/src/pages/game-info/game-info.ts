a7bcc91c398a7a4ad50e834e2ba6d35e5c5327b0
christinechengubc <christine.lyn.cheng@gmail.com>
2018-03-25 18:33:25 -0700

    implemented games info page

--- client/src/pages/game-info/game-info.ts
+++ client/src/pages/game-info/game-info.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the GamesPage page.
@@ -15,7 +17,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GameInfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  game: any = {};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public translateService: TranslateService) {
+    translateService.get('DATABASE_URL').subscribe(value => {
+      var title = navParams.data.gameTitle.trim();
+      console.log(title);
+      this.http.get(value + "/game-info/'" + title + "'").map(res => res.json()).subscribe(
+        data => {
+          this.game = data.data[0];
+          console.log("game variable is: ");
+          console.log(this.game);
+        },
+        err => {
+          console.log("Oops!");
+          console.log(err);
+        }
+      );
+    });
   }
 
   editGame() {
