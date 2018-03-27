f4a6dc7438a8626abaf69d2d763dc5ee11367827
christinechengubc <christine.lyn.cheng@gmail.com>
2018-03-26 17:30:24 -0700

    implement most of statistics page except backend events query

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -5,6 +5,7 @@ var gameinfo = require('./game-info');
 var gamecreate = require('./game-create');
 var eventinfo = require('./event-info');
 var eventcreate = require('./event-create');
+var stats = require('./stats.js');
 
 routes.use('/games', games);
 routes.use('/events', events);
@@ -12,6 +13,7 @@ routes.use('/game-info', gameinfo);
 routes.use('/game-create', gamecreate);
 routes.use('/event-info', eventinfo);
 routes.use('/event-create', eventcreate);
+routes.use('/stats', stats);
 
 routes.get('/', (req, res) => {
 	res.status(200).json({ message: "Connected!"});
