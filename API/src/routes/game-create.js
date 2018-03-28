2f74ff8641309436325acff32eed1fee2829d0d8
terbb <trevinwong@gmail.com>
2018-03-28 14:14:24 -0700

    Feature to create games and POST them now implemented.

--- API/src/routes/game-create.js
+++ API/src/routes/game-create.js
@@ -42,32 +42,42 @@ gamecreate.get('/publishers', (req, res) => {
 
 gamecreate.post('/new', (req, res) => {
 	var sql = new PQ('INSERT INTO Games VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)');
-	sql.values = [req.body.title, req.body.rating, req.body.minplayers, req.body.maxplayers, req.body.minplaytime, req.body.maxplaytime, req.body.yearpublished, req.body.description, req.body.difficulty];
+	sql.values = [req.body.title, req.body.rating, req.body.minPlayers, req.body.maxPlayers, req.body.minPlaytime, req.body.maxPlaytime, req.body.yearPublished, req.body.description, req.body.difficulty];
 	var sql2 = new PQ('INSERT INTO PublishedBy VALUES($1, $2)');
-	sql2.values = [];
 	var sql3 = new PQ('INSERT INTO HasGenre VALUES($1, $2)');
-	sql3.values = [];
+	var sql4 = new PQ('SELECT max(id) as id FROM GameInstances WHERE gameTitle = $1');
+	sql4.values = [req.body.title];
+	var sql5 = new PQ('INSERT INTO GameInstances VALUES($1, $2, $3, $4, $5)');
 
 	var result = {};
 
 	db.task(t => {
 		return t.none(sql)
 			.then(() => {
-					var promises = [];
-					req.body.publishers.forEach((publisher) => {
-						sql2.values = [publisher, req.body.title];
-						promises.push(t.none(sql2));
-					});
+				return t.one(sql4)
+					.then((mostRecentGameInstance) => {
+						var nextID;
+						if (mostRecentGameInstance.id === null) nextID = 0;
+						else nextID = mostRecentGameInstance.id + 1;
+						// actually redundant since users should never be allowed to create duplicate games, hence mostRecentGameInstance.id will always equal 0.
+						// but i already wrote the query and i want to re-use the code for something else, e.g. adding a new game instance
 
-					req.body.genres.forEach((genre) => {
-						sql3.values = [req.body.title, genre];
-						promises.push(t.none(sql3));
-					});
+						var promises = [];
+						sql5.values = [nextID, 0, req.body.datePurchased, req.body.language, req.body.title];
+						promises.push(t.none(sql5));
 
-					return Promise.all(promises);
-			})
-			.catch((error) => {
-				console.error(error);
+						req.body.publishers.forEach((publisher) => {
+							sql2.values = [publisher, req.body.title];
+							promises.push(t.none(sql2));
+						});
+
+						req.body.genres.forEach((genre) => {
+							sql3.values = [req.body.title, genre];
+							promises.push(t.none(sql3));
+						});
+
+						return Promise.all(promises);
+					});
 			})
 	})
 		.then(() => {
@@ -78,6 +88,10 @@ gamecreate.post('/new', (req, res) => {
 		})
 		.catch((error) => {
 			console.error(error);
+			res.status(500)
+				.json({
+					status: 'failure'
+				});
 		});
 });
 
