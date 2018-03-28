d10c6195f5b33bab3535201ee4a07b1a2a4817e0
Bryce <n9j0b@ugrad.cs.ubc.ca>
2018-03-27 19:07:58 -0700

    delete and update for games and events

--- API/src/routes/borrowed.js
+++ API/src/routes/borrowed.js
@@ -1,14 +1,13 @@
 var games = require('express').Router();
 var db = require('../db');
-
 var PQ = require('pg-promise').ParameterizedQuery;
 var dh = require('./dataHandler');
 
 games.get('/', (req, res) => {
-	var sql = 'SELECT title, rating, name as publisher ' +
-  'FROM games, publishedby, publishers ' +
-  'WHERE games.title = publishedby.gameTitle AND publishers.name = publishedby.publisherName ' +
-  'ORDER BY title';
+  var sql = 'SELECT title, rating, name as publisher ' +
+    'FROM games, publishedby, publishers ' +
+    'WHERE games.title = publishedby.gameTitle AND publishers.name = publishedby.publisherName ' +
+    'ORDER BY title';
   db.any(sql)
     .then(function (data) {
       //returns JSON with list of distinct titles and array of publishers in the publisher field
@@ -22,47 +21,8 @@ games.get('/', (req, res) => {
         });
     })
     .catch(function (err) {
-			console.error("Error when retrieving games " + err);
-		});
-});
-
-games.get('/has-genre/:genre', (req, res) => {
-  var genre = req.params.genre;
-  var sql = 'SELECT gametitle FROM games, hasgenre WHERE gametitle = title AND genrename =' + genre;
-db.any(sql)
-  .then(function (data) {
-
-    res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved games with genre ' + genre
-      });
-  })
-  .catch(function (err) {
-    console.error("Error when retrieving games " + err);
-  });
-});
-
-games.get('/title/:partTitle', (req, res) => {
-  var partTitle = req.params.partTitle;
-  partTitle = partTitle.replace(/\'/g, "");
-
-var sql = 'SELECT title FROM games WHERE title LIKE \'%' + partTitle + '%\'';
-db.any(sql)
-  .then(function (data) {
-
-    res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved games with partial title ' + partTitle
-      });
-  })
-  .catch(function (err) {
-    console.log(sql);
-    console.error("Error when retrieving games " + err);
-  });
+      console.error("Error when retrieving games " + err);
+    });
 });
 
 games.put('/edit', (req, res) => {
--- API/src/routes/events.js
+++ API/src/routes/events.js
@@ -1,5 +1,6 @@
 var events = require('express').Router();
 var db = require('../db');
+var PQ = require('pg-promise').ParameterizedQuery;
 
 //pass date as all numbers as string in form 'DDMMYYYY'
 events.get('/:title/:date', (req, res) => {
@@ -25,26 +26,44 @@ events.get('/:title/:date', (req, res) => {
 		});
 });
 
-events.put('/add/:name/:date/:description/:startTime/:endTime/:location', (req,res) => {
+events.put('/edit', (req,res) => {
   var name = req.body.name;
   var date = req.body.date;
   var description = req.body.description;
   var startTime = req.body.startTime;
   var endTime = req.body.endTime;
   var location = req.body.location;
-  var value = name + ',' + date + ',' + description + ',' + startTime + ',' + endTime + ',' + location;
-  var sql = 'INSERT INTO events VALUES(' + value + ')';
+
+  var sql = new PQ('UPDATE events ' +
+    'SET name = $1, date = $2, description = $3, startTime = $4, endTime = $5, location = $6 ' +
+    'WHERE name = $1 AND date = $2');
+  sql.values = [name,date,description,startTime,endTime,location];
 
   db.none(sql).then(
     res.status(200)
       .json({
         status: 'success',
-        message: 'Added the event to Events'
+        message: 'Updated event successfully'
       })
   ).catch(function (err){
-    console.error("Error adding event to Events: " + err);
+    console.error("Error updating event : " + err);
   })
 });
 
+events.delete('/del/:name', (req, res) => {
+  var name = req.params.name;
+  console.log(name);
+  var sql = new PQ('DELETE FROM events WHERE name = $1');
+  sql.values = [name];
+  db.none(sql).then(
+    res.status(200)
+      .json({
+        status: 'success',
+        message: 'Deleted event successfully'
+      })
+  ).catch(function(err){
+    console.error("Error deleting: " + err);
+  })
+});
 
 module.exports = events;
