1fd5c9ef72b529513d6474bcc440038e32a2d4a7
terbb <trevinwong@gmail.com>
2018-03-29 22:09:00 -0700

    Added error handling front end

--- client/src/providers/providers.ts
+++ client/src/providers/providers.ts
@@ -2,10 +2,12 @@ import { Api } from './api/api';
 import { Items } from '../mocks/providers/items';
 import { Settings } from './settings/settings';
 import { User } from './user/user';
+import { SanitizerProvider } from './sanitizer/sanitizer';
 
 export {
     Api,
     Items,
     Settings,
-    User
+    User,
+    SanitizerProvider
 };
