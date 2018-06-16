c7d69c2788134fb031a723a5ea88b52bd7860e55
terbb <trevinwong@gmail.com>
2018-06-16 15:29:05 -0700

    Change the way connection info is handled

--- API/src/db.js
+++ API/src/db.js
@@ -1,10 +1,14 @@
-const initOptions = {
-
-};
+var connection_info;
+try {
+  connection_info = require('./connection_info');
+} catch (err) {
+  connection_info = process.env.DATABASE_URL;
+}
+const initOptions = {};
 
 const pgp = require('pg-promise')(initOptions);
 
-const cn = process.env.DATABASE_URL;
+const cn = connection_info;
 const db = pgp(cn);
 
 module.exports = db;
