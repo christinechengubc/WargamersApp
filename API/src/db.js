12e2ddfa9647216bb7998fbda08e98506b2749c1
Christine Cheng <christine@formathletica.com>
2018-06-18 15:57:00 -0700

    deleted unnecessary files

--- API/src/db.js
+++ API/src/db.js
@@ -4,7 +4,7 @@ const initOptions = {
 
 const pgp = require('pg-promise')(initOptions);
 
-const cn = process.env.DATABASE_URL;
+const cn = 'postgres://christine:@localhost:5432/christine';;
 const db = pgp(cn);
 
 module.exports = db;
