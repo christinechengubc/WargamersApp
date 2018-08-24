2f062d4dc2990082b43ba6ab83b9dd611bdbf649
crystal-ck7 <crystal.ck7@gmail.com>
2018-08-24 14:57:45 -0600

    edited some changes and cleaned up code

--- API/src/db.js
+++ API/src/db.js
@@ -2,7 +2,7 @@ var connection_info;
 try {
   connection_info = require('./connection_info');
 } catch (err) {
-  connection_info = 'postgres://cryst:Spirals@localhost:5432/cryst';//process.env.DATABASE_URL;
+  connection_info = process.env.DATABASE_URL;
 }
 const initOptions = {};
 
