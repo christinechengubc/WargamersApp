5df3588f72bb283e509ea69ef1699ef586fea82b
christinechengubc <christine.lyn.cheng@gmail.com>
2018-09-09 11:45:04 -0700

    change side menu options

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -32,16 +32,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   pages: any[] = [
-    { title: 'Tutorial', component: 'TutorialPage' },
-    { title: 'Welcome', component: 'WelcomePage' },
-    { title: 'Tabs', component: 'TabsPage' },
-    { title: 'Cards', component: 'CardsPage' },
-    { title: 'Content', component: 'ContentPage' },
-    { title: 'Login', component: 'LoginPage' },
-    { title: 'Signup', component: 'SignupPage' },
-    { title: 'Master Detail', component: 'ListMasterPage' },
-    { title: 'Menu', component: 'MenuPage' },
-    { title: 'Settings', component: 'SettingsPage' },
+    { title: 'Main', component: 'TabsPage' },
     { title: 'Search', component: 'SearchPage' },
   ]
 
