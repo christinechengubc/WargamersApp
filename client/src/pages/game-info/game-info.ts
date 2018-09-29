ce1789a927d9c2cef92c411d4bfa2150e1a372b3
crystal-ck7 <crystal.ck7@gmail.com>
2018-09-29 12:00:04 -0700

    edit/add/delete buttons will now error and disappear if clicked and you're not logged in

--- client/src/pages/game-info/game-info.ts
+++ client/src/pages/game-info/game-info.ts
@@ -7,6 +7,7 @@ import { GameEditPage } from '../game-edit/game-edit';
 import { Api } from '../../providers/providers';
 import { Storage } from "@ionic/storage";
 import {HttpHeaders} from "@angular/common/http";
+import {GamesPage} from "../games/games";
 
 /**
  * Generated class for the GamesPage page.
@@ -24,17 +25,40 @@ export class GameInfoPage {
 
   game: any = {};
   token: any;
+  login = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public user: User, public api: Api, public toastCtrl: ToastController, public events: Events, public storage: Storage, private alertCtrl: AlertController) {
     this.storage.get('token').then((token) => {
       this.token = token;
-    })
+    });
+    this.storage.get('login').then((login) => {
+      this.login = login;
+    });
   }
 
   editGame() {
-    this.navCtrl.push('GameEditPage', {
-      game: this.game,
-    });
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token
+      })
+    };
+
+    this.api.get('login/', null, httpOptions).subscribe(
+      resp => {
+        this.navCtrl.push('GameEditPage', {
+          game: this.game,
+        });
+      },
+      err => {
+        this.storage.set('login', 0);
+        let toast = this.toastCtrl.create({
+          message: 'Cannot edit game. Error: not logged in',
+          duration: 3000,
+          position: 'top'
+        });
+        toast.present();
+      }
+    )
   }
 
   deleteGame() {
@@ -57,12 +81,15 @@ export class GameInfoPage {
       },
       err => {
         console.log(err);
+        this.storage.set('login', 0);
         let toast = this.toastCtrl.create({
-          message: 'Failed to delete game from database. Error: ' + err.error.detail,
+          message: 'Failed to delete game from database. Error: not logged in',
           duration: 3000,
           position: 'top'
         });
         toast.present();
+        this.events.publish('refresh');
+        this.navCtrl.setRoot(GamesPage);
       }
     )
   }
@@ -88,6 +115,10 @@ export class GameInfoPage {
       ]
     });
     alert.present();
+  }
+
+  isLoggedIn() {
+    return this.login;
   }
 
   ionViewDidLoad() {
