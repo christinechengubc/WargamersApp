a70c57b793a248fadb968a4dfbf06f5e62b8d173
christinechengubc <christine.lyn.cheng@gmail.com>
2018-10-16 11:57:27 -0700

    Commented out more admin API

--- API/src/routes/admins.js
+++ API/src/routes/admins.js
@@ -36,33 +36,33 @@ const saltRounds = 10;
 //       });
 //   });
 // });
-
-
-admins.delete('/:id', (req, res) => {
-  var sql = new PQ('DELETE FROM app_admins WHERE id = $1');
-  sql.values = [req.params.id];
-
-  db.none(sql)
-		.then(() => {
-	    res.status(200)
-				.json({
-					status: 'ok',
-					code: 200,
-					message: 'Deleted admin with id: ' + req.params.id,
-					result: {}
-				});
-		})
-		.catch((err) => {
-			console.error('\n[ERROR]: GET /admins/:id\n');
-			console.error(err);
-			res.status(500)
-				.json({
-					status: 'error',
-					code: 500,
-					message: err.message
-				});
-	  });
-});
+// 
+//
+// admins.delete('/:id', (req, res) => {
+//   var sql = new PQ('DELETE FROM app_admins WHERE id = $1');
+//   sql.values = [req.params.id];
+//
+//   db.none(sql)
+// 		.then(() => {
+// 	    res.status(200)
+// 				.json({
+// 					status: 'ok',
+// 					code: 200,
+// 					message: 'Deleted admin with id: ' + req.params.id,
+// 					result: {}
+// 				});
+// 		})
+// 		.catch((err) => {
+// 			console.error('\n[ERROR]: GET /admins/:id\n');
+// 			console.error(err);
+// 			res.status(500)
+// 				.json({
+// 					status: 'error',
+// 					code: 500,
+// 					message: err.message
+// 				});
+// 	  });
+// });
 
 
 module.exports = admins;
